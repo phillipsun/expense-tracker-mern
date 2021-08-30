8e7703f9709f6412cafbff6cb30e2f400776ed46
Phil Sun <psun.04@gmail.com>
2021-08-29 22:00:35 -0500

    first commit

---
+++ client/src/components/IncomeExpenses.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format'
+
+export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amount = transactions.map((transaction) => transaction.amount);
+
+  const income = amount
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amount.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  return (
+    <div className='inc-exp-container'>
+      <div>
+        <h4>Income</h4>
+        <p id='money-plus' className='money plus'>
+          ${numberWithCommas(income)}
+        </p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id='money-minus' className='money minus'>
+          ${numberWithCommas(expense)}
+        </p>
+      </div>
+    </div>
+  );
+};
